d2a7c3e970ebab06616d03efd31da232aa0d5b43
Dennis Nesanoff <nndennisnn@gmail.com>
2019-12-15 13:21:44 +0300

    Initial Commit

---
+++ Magic Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Ball
+//
+//  Created by Dennis Nesanoff on 15.12.2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
